67f2adc653861e112164e2740db116e3e7d258ed
AlexisLebreton <AlexisLebreton@users.noreply.github.com>
2019-03-22 17:22:10 +0100

    membrerestcontroller maj

--- Rando/RandoMembre/src/main/java/fr/miage/randomembre/metier/GestionMembre.java
+++ Rando/RandoMembre/src/main/java/fr/miage/randomembre/metier/GestionMembre.java
@@ -8,6 +8,7 @@ package fr.miage.randomembre.metier;
 import fr.miage.randomembre.entities.Membre;
 import fr.miage.randomembre.repositories.MembreInterface;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -16,9 +17,19 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class GestionMembre {
     @Autowired
-    private MembreInterface membInterface;
+    private MembreInterface membreInterface;
     
-    public List<Membre> getAllMembres(){
-        return (List<Membre>) membInterface.findAll();
+    public List<Membre> getMembres(){
+        return (List<Membre>) membreInterface.findAll();
     }
+
+    public List<Membre> getMembre(Long id) {
+        Optional<Membre> membreReturn =  this.membreInterface.findById(id);
+        if(!membreReturn.isPresent()){
+            
+        }
+        return  (List<Membre>) membreReturn.get();
+    }
+    
+    
 }
