dc157cd4bd79bee973b39fc3d120b998a4d26da3
AlexisLebreton <AlexisLebreton@users.noreply.github.com>
2019-06-17 16:49:55 +0200

    association mieux géré

--- Rando/RandoMembre/src/main/java/fr/miage/randomembre/metier/GestionAssociation.java
+++ Rando/RandoMembre/src/main/java/fr/miage/randomembre/metier/GestionAssociation.java
@@ -8,6 +8,7 @@ package fr.miage.randomembre.metier;
 import fr.miage.randomembre.entities.Association;
 import fr.miage.randomembre.entities.Membre;
 import fr.miage.randomembre.repositories.AssociationInterface;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -23,26 +24,32 @@ public class GestionAssociation {
     private AssociationInterface associationInterface;
 
     public Association getAssociation() {
-        Optional<Association> assoReturn = this.associationInterface.findById((long) 1);
-        if (!assoReturn.isPresent()) {
-            //TODO exception 
+        List<Association> listAssos = (List<Association>) associationInterface.findAll();
+        if (listAssos.size() != 0){
+            return listAssos.get(0);
+        }else{
+            return null;
         }
-        return assoReturn.get();
+        
     }
     
      public Association createAssociation(Association association) {
-         
-        return this.associationInterface.save(association);
+        List<Association> listAssos = (List<Association>) associationInterface.findAll();
+        if (listAssos.size() == 0){
+            return this.associationInterface.save(association);
+        }else{
+            return null;
+        }
     }
      
      public void financerRando(float cout) {
-        Optional<Association> assoReturn = this.associationInterface.findById((long) 1);
-        if (!assoReturn.isPresent()) {
-            //TODO exception 
+         List<Association> listAssos = (List<Association>) associationInterface.findAll();
+        if (listAssos.size() != 0){
+            Association a = listAssos.get(0);
+            a.setBudgetAsso(a.getBudgetAsso() - cout);
+            this.associationInterface.save(a);
         }
-        Association a = assoReturn.get();
-        a.setBudgetAsso(a.getBudgetAsso() - cout);
-        this.associationInterface.save(a);
+        
     }
 
     public String reporting() {
