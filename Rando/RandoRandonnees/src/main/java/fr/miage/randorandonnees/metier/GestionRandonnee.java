c32230ecee00d834b3b6b974074a76b4024c1936
chaouch.maha29@gmail.com <maha-chaouch@hotmail.fr>
2019-06-27 11:33:24 +0200

    doc

--- Rando/RandoRandonnees/src/main/java/fr/miage/randorandonnees/metier/GestionRandonnee.java
+++ Rando/RandoRandonnees/src/main/java/fr/miage/randorandonnees/metier/GestionRandonnee.java
@@ -62,11 +62,10 @@ public class GestionRandonnee {
         Date today = new Date();
         Boolean datesValides = rando.getDate1().equals(today) && rando.getDate2().equals(today) && rando.getDate3().equals(today);
 
-         
         //organisateur apte : vérifier que la personne qui veut créer la rando est un organisateur + il a un niveau 1,5 ...
         Long idTL = rando.getIdTeamLeader();
         int niveauTL = 0;
-        int  niveauRando = rando.getNiveauCible();
+        int niveauRando = rando.getNiveauCible();
         RestTemplate restTemplate = new RestTemplate();
         String fooResourceUrl = "http://localhost:8080/api/randoMembre/";
         ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl + idTL, String.class);
@@ -82,20 +81,18 @@ public class GestionRandonnee {
             Logger.getLogger(GestionRandonnee.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        if (niveauRando >= niveauTL){
+        if (niveauRando >= niveauTL) {
             return null;
-        } 
-            
-       
+        }
+
         //verif cout
         float budgetAssociation = 0;
         float budgetRando = rando.getCoutFixeR();
-        
+
         RestTemplate restTemplate2 = new RestTemplate();
         String fooResourceUrl2 = "http://localhost:8080/api/randoAsso";
         ResponseEntity<String> response2 = restTemplate2.getForEntity(fooResourceUrl2, String.class);
 
-        
         ObjectMapper mapper2 = new ObjectMapper();
         JsonNode root2;
         try {
@@ -107,17 +104,16 @@ public class GestionRandonnee {
             Logger.getLogger(GestionRandonnee.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        if (budgetAssociation < budgetRando){
+        if (budgetAssociation < budgetRando) {
             return null;
         }
-        
+
         /*
         //actualiser tresorerie
         RestTemplate restTemplate3 = new RestTemplate();
         String fooResourceUrl3 = "http://localhost:8080/api/randoAsso/financerRando?cout="+rando.getCoutFixeR();
         ResponseEntity<String> response3 = restTemplate3.getForEntity(fooResourceUrl3, String.class);
-        */
-        
+         */
         Randonnee initRando = new Randonnee(rando.getTitreR(), rando.getNiveauCible(), rando.getIdTeamLeader(), rando.getLieuR(), rando.getDistanceR(), rando.getCoutFixeR(), rando.getCoutVariableR(), rando.getDate1(), rando.getDate2(), rando.getDate3());
         //System.out.println(initRando.toString());
         return this.randoInterface.save(initRando);
@@ -161,6 +157,12 @@ public class GestionRandonnee {
         }
     }
 
+    /**
+     * Permet au team leader de cloturer une inscription à une rando dont l'id
+     * est passé en paramètre
+     *
+     * @param id
+     */
     public void cloturerInscription(String id) {
 
         Randonnee randoReturn = this.randoInterface.findById(id).get();
@@ -173,48 +175,57 @@ public class GestionRandonnee {
 
             //fixer la date de la rando selon les votes
             String dateElue = dateApresVotes(randoReturn);
-            System.out.println("date elue: "+ dateApresVotes(randoReturn));            
+            System.out.println("date elue: " + dateApresVotes(randoReturn));
             randoReturn.setDateRando(dateElue);
-            
+
             //affacter toute personne ayant voté pour la date "finale" de la rando après cloture
             inscrireVoteurs(randoReturn);
-            
+
             //inscrire automatiquement le TL
             List<Long> idInscris = randoReturn.getListInscris();
-            if(!idInscris.contains(randoReturn.getIdTeamLeader())){
+            if (!idInscris.contains(randoReturn.getIdTeamLeader())) {
                 idInscris.add(randoReturn.getIdTeamLeader());
             }
-            
+
             //màj le budget de l'association
-            //TODO
-            
             //màj de la rando
             randoInterface.save(randoReturn);
         }
     }
-    
-    public void inscrireVoteurs(Randonnee rando){
-            HashMap votes = rando.getVotesR();
-           
-            ArrayList<Long> idVoteurs =  (ArrayList<Long>) votes.get(rando.getDateRando());            
-            List<Long> idInscris = rando.getListInscris();
-            
-            for (int i=0; i< idVoteurs.size();i++){
-                
-                //vérifier si le membre qui a voté n'est pas déja inscri
-                Long idVoteur = idVoteurs.get(i);
-                if(!idInscris.contains(idVoteur)){
-                    //ajouter le membre à la liste des inscris
-                    idInscris.add(idVoteur);
-                }
-            }  
-    }
-    
-    public void modifierBudgetAssociation(){
-        
-    } 
 
-    //détermine la date qui a gagné les votes du sondage
+    /**
+     * Après une cloture d'inscription: permet d'inscrire automatiquement tous
+     * ceux qui ont voté pour la date élue
+     *
+     * @param rando
+     */
+    public void inscrireVoteurs(Randonnee rando) {
+        HashMap votes = rando.getVotesR();
+
+        ArrayList<Long> idVoteurs = (ArrayList<Long>) votes.get(rando.getDateRando());
+        List<Long> idInscris = rando.getListInscris();
+
+        for (int i = 0; i < idVoteurs.size(); i++) {
+
+            //vérifier si le membre qui a voté n'est pas déja inscri
+            Long idVoteur = idVoteurs.get(i);
+            if (!idInscris.contains(idVoteur)) {
+                //ajouter le membre à la liste des inscris
+                idInscris.add(idVoteur);
+            }
+        }
+    }
+
+    public void modifierBudgetAssociation() {
+
+    }
+
+    /**
+     * Détermine la date qui a le plus de votes
+     *
+     * @param r
+     * @return la date Elue
+     */
     public String dateApresVotes(Randonnee r) {
         //get Max votes 
         Integer nbVotes1 = r.getVotesR().get(r.getDate1()).size();
@@ -237,6 +248,13 @@ public class GestionRandonnee {
         return null;
     }
 
+    /**
+     * Permet à un membre de voter à un créneau d'une randonnée
+     * @param idRando
+     * @param idMembre
+     * @param dateChoisie
+     * @throws IOException 
+     */
     public void voterCreneau(String idRando, long idMembre, String dateChoisie) throws IOException {
 
         //chercher la rando
@@ -270,8 +288,12 @@ public class GestionRandonnee {
             }
         }
     }
-
-    //inscription d un membre à une rando
+/**
+ * Permet à un membre de s'inscrire à une rando
+ * @param idRando
+ * @param idMembre 
+ */
+    
     public void inscriptionRando(String idRando, long idMembre) {
         //chercher la rando
 
@@ -301,7 +323,12 @@ public class GestionRandonnee {
         }
     }
 
-    //renvoie la list des randos aux quelles le membre ne s est pas inscri
+/**
+ * Renvoie la list des randos aux quelles le membre ne s est pas inscri (inscription non cloturée)
+ * @param idMembre
+ * @return
+ * @throws IOException 
+ */    
     public List<Randonnee> getRandoInscriNonCloturePourUnMembre(Long idMembre) throws IOException {
 
         //vérifier que le membre existe
@@ -321,7 +348,7 @@ public class GestionRandonnee {
         //vérifier si le membre est apte avant de s inscrire
         if (isApte.asBoolean()) {
 
-            List<Randonnee> randosInscisOuvertes = this.randoInterface.findByInscriClotureAndSondageCloture(false,true);
+            List<Randonnee> randosInscisOuvertes = this.randoInterface.findByInscriClotureAndSondageCloture(false, true);
 
             for (int i = 0; i < randosInscisOuvertes.size(); i++) {
                 Randonnee rando = randosInscisOuvertes.get(i);
@@ -339,26 +366,11 @@ public class GestionRandonnee {
         }
         return randosToReturn;
     }
-
- /*   public List<Randonnee> getRandoPassees() {
-        List<Randonnee> randoFinished = new ArrayList<Randonnee>();
-        List<Randonnee> randoReturn = this.randoInterface.findAll();
-
-        for (int i = 0; i < randoReturn.size(); i++) {
-            Date today = new Date();
-            Randonnee r = randoReturn.get(i);
-            if (r.getDateRando().compareTo(today) > 0) {
-                randoFinished.add(r);
-            }
-        }
-        return randoFinished;
-    } */
-
-    public List<Randonnee> getCouTotalRandos() {
-        //TO DO
-        return null;
-    }
-
+    /**
+     * Transfomre le retour data mondo en un String
+     * @param randos
+     * @return 
+     */
     public String convertDataToString(List<Randonnee> randos) {
         String result = "[";
         for (int i = 0; i < randos.size(); i++) {
@@ -372,16 +384,16 @@ public class GestionRandonnee {
 
     public List<Randonnee> getRandoInscriNonCloture() {
         return this.randoInterface.findByInscriCloture(false);
-    } 
+    }
 
-        public List<Randonnee> getRandoVotesNonClotureCréeParUnTL(Long idTL) {
+    public List<Randonnee> getRandoVotesNonClotureCréeParUnTL(Long idTL) {
         return this.randoInterface.findByIdTeamLeaderAndSondageCloture(idTL, false);
     }
-        
+
     public List<Randonnee> getRandoInsciNonClotureCréeParUnTL(Long idTL) {
         return this.randoInterface.findByIdTeamLeaderAndInscriCloture(idTL, false);
     }
-        
+
     public List<Randonnee> getRandoVoteNonCloture() {
         return this.randoInterface.findBySondageCloture(false);
     }
@@ -395,11 +407,11 @@ public class GestionRandonnee {
             HashMap votes = allRandoWithOpenVotes.get(i).getVotesR();
             Iterator it = votes.entrySet().iterator();
 
-            //parcourir les votes de chaques rando: chaque ligne dans la map sera appele vote = <date, arrayList<idMember>>
+            //parcourir les votes de chaque rando: chaque ligne dans la map sera appele vote = <date, arrayList<idMember>>
             Boolean exist = false;
             while (it.hasNext() && !exist) {
                 HashMap.Entry vote = (HashMap.Entry) it.next();
-                // System.out.println(vote.getKey() + " = " + vote.getValue());
+
                 ArrayList<Long> idMembers = (ArrayList<Long>) vote.getValue();
 
                 //vérifier si l id du membre existe dans une des 3 liste de votes
@@ -428,14 +440,14 @@ public class GestionRandonnee {
             totalcoutrando += randonnee.getCoutFixeR();
             int nbparticipant = randonnee.getListInscris().size();
             float coutvar = randonnee.getCoutVariableR();
-            totalcoutrando += nbparticipant*coutvar;
-            
+            totalcoutrando += nbparticipant * coutvar;
+
             if (randonnee.getDateRando() != null) {
                 nbRandoPos++;
             } else {
                 encour += randonnee.getCoutFixeR();
             }
         }
-        return "{\"totalCoutRando\" : \""+totalcoutrando+"\", \"nbRandoPos\" : \""+nbRandoPos+"\", \"encour\" : \""+encour+"\"  }";
+        return "{\"totalCoutRando\" : \"" + totalcoutrando + "\", \"nbRandoPos\" : \"" + nbRandoPos + "\", \"encour\" : \"" + encour + "\"  }";
     }
 }
